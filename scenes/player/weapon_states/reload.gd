b605518328750d5c1f491693c9e71ed968375ccb
dragon1freak <dragon1freak2@gmail.com>
2025-01-22 21:15:47 -0500

    Added reload sounds

--- scenes/player/weapon_states/reload.gd
+++ scenes/player/weapon_states/reload.gd
@@ -10,7 +10,18 @@ func enter(_t) -> void:
 func do_reload() -> void:
 	# Play reload sounds
 	
-	await get_tree().create_timer(current_weapon.reload_time).timeout
+	if current_weapon.sound_reload_start:
+		Audio.play(current_weapon.sound_reload_start, "SFX")
+	
+	await get_tree().create_timer(current_weapon.reload_time / 2.0).timeout
+	
+	if current_weapon.sound_reload_step:
+		Audio.play(current_weapon.sound_reload_step, "SFX")
+	
+	await get_tree().create_timer(current_weapon.reload_time / 2.0).timeout
+	
+	if current_weapon.sound_reload_end:
+		Audio.play(current_weapon.sound_reload_end, "SFX")
 	
 	current_weapon.current_magazine = current_weapon.max_magazine_size
 	
--- scenes/player/weapon_states/shoot.gd
+++ scenes/player/weapon_states/shoot.gd
@@ -11,7 +11,11 @@ func do_shoot() -> void:
 	if current_weapon.sound_shoot:
 		Audio.play(current_weapon.sound_shoot, "SFX")
 	current_weapon.current_magazine -= 1
+	
 	await get_tree().create_timer(current_weapon.fire_rate).timeout
+	
+	if current_weapon.sound_post_shoot:
+		Audio.play(current_weapon.sound_post_shoot, "SFX")
 	
 	if current_weapon.current_magazine <= 0:
 		return transition("reload")
